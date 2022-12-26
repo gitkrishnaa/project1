d54c3f1f4455696e43b60cb70c017ea5eaf66dbf
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2022-12-27 00:22:31 +0530

    1 file modified, 2 added

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello");
+
+let projectArr=[]
+projectArr.push("https://apna-market.netlify.app/")
+
+let display=document.getElementById('project');
+
+let div1=document.createComment("div");
+display.appendChild(div1);
+
+iframe.inner
