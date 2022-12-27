828826cec354f61d72d2d721cc76bf17d9b8e2a2
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2022-12-27 09:22:30 +0530

    2 files modified

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ projectArr.push("https://apna-market.netlify.app/")
 
 let display=document.getElementById('project');
 
-let div1=document.createComment("div");
-display.appendChild(div1);
+let div1=document.createElement("iframe")
+display.appendChild(div1)
+ div1.setAttribute("src",projectArr[0])
+ div1.style.height="350px"
+ div1.style.width="50vw"
+ div1.style.borderRadius="10px"
 
-iframe.inner
+
